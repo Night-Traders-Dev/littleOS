39bd899d9f172e4893dddf20ebcb1d4a4bcea918
Night-Traders-Dev <80061955+Night-Traders-Dev@users.noreply.github.com>
2025-12-01 23:31:47 -0500

    Fix: Add missing parser.h, ast.h, and value.h includes

--- src/sage_embed.c
+++ src/sage_embed.c
@@ -5,7 +5,10 @@
 
 // Include SageLang headers (use proper CMake include directories)
 #include "lexer.h"
+#include "parser.h"
+#include "ast.h"
 #include "interpreter.h"
+#include "value.h"
 #include "env.h"
 #include "gc.h"
 
